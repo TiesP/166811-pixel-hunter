461a6247c7d2e9f75727b67b2fd1417bd9b11571
TiesP <pavel.o.71@gmail.com>
2017-06-02 16:19:28 +0300

    убрал лишние пробелы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { handlerChangePageTemplate, initial } from './utils.js';
+import {handlerChangePageTemplate, initial} from './utils.js';
 import greeting from './greeting';
 import rules from './rules';
 import gameOne from './game-1';
@@ -9,4 +9,4 @@ import stats from './stats';
 const intro = document.querySelector(`.intro__asterisk`);
 intro.addEventListener(`click`, handlerChangePageTemplate(`greeting`));
 
-initial({ 'greeting': greeting, 'rules': rules, 'game-1': gameOne, 'game-2': gameTwo, 'game-3': gameThree, 'stats': stats });
+initial({'greeting': greeting, 'rules': rules, 'game-1': gameOne, 'game-2': gameTwo, 'game-3': gameThree, 'stats': stats});
