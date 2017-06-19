69b2c56fec3a462ae3b2e2226a85a12dbcafb931
TiesP <pavel.o.71@gmail.com>
2017-06-19 07:45:08 +0300

    настройка тестирования

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const rollup = require('gulp-better-rollup');
 const sourcemaps = require('gulp-sourcemaps');
+const mocha = require('gulp-mocha'); // Добавим установленный gulp-mocha плагин
 
 gulp.task('style', function() {
   gulp.src('sass/style.scss')
@@ -37,7 +38,15 @@ gulp.task('style', function() {
     .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('test', function() {});
+gulp.task('test', function () {
+  return gulp
+    .src(['js/**/*.test.js'], { read: false })
+    .pipe(mocha({
+      compilers: ['js:babel-register'], // Включим поддержку "import/export" в Mocha тестах
+      reporter: 'spec'       // Вид в котором я хочу отображать результаты тестирования
+    }));
+});
+
 gulp.task('scripts', function() {
   return gulp.src('js/main.js')
     .pipe(plumber())
